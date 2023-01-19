8f9246b8923bbc899ed1ab1d1e57857e1c514135
Nelia Bochenkova <nelya.teslenko.95@gmail.com>
2023-01-19 23:54:15 +0200

    add

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,4 +2,4 @@ export const selectContact = state => state.contacts.items;
 
 export const selectFilter = state => state.filter;
 export const selectIsLoading = state => state.contacts.isLoading;
-export const selectError = state => state.contacts.error;
+export const selectError = state => state.error;
